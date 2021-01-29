0c2344dcd7561c8cc3f212a4f44a2974f738d81e
root <root@DESKTOP-K4RHFBE.localdomain>
2021-01-29 19:19:25 +0100

    update chef json

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="">
+        <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/getDataFromApi.js
@@ -0,0 +1,86 @@
+[
+    {
+        "id": 0,
+        "name": "David Montero",
+        "restaurant": "Rice Paella",
+        "recipe": "Arroz de solomillo con pasas y orejones",
+        "website": "https://rice-paella.com/",
+        "chefImageName": "davidmontero.jpg",
+        "recipeImageName": "davidmontero_receta.jpg",
+        "video": "zhWtvFhLHvY"
+    },
+    {
+        "id": 1,
+        "name": "Fernando del Cerro",
+        "restaurant": "Restaurante Casa Jose",
+        "recipe": "Lomo de buey a la sal negra",
+        "website": "http://casajose.es/",
+        "chefImageName": "fernandodelcerro.jpg",
+        "recipeImageName": "fernandodelcerro_receta.jpg",
+        "video": "x2LtwRnTEUc"
+        
+    },
+    {
+        "id": 2,
+        "name": "Fran Martínez",
+        "restaurant": "Restaurante Maralba",
+        "recipe": "Canelones de carrillada con foie gras",
+        "website": "https://www.maralbarestaurante.es/",
+        "chefImageName": "franmartinez.jpg",
+        "recipeImageName": "franmartinez_receta.jpg",
+        "video": "-eelhpeHfJA"
+    },
+    {
+        "id": 3,
+        "name": "Iñigo Urrechu",
+        "restaurant": "Restaurante Urrechu",
+        "recipe": "Cachopo",
+        "website": "https://urrechuvelazquez.com/",
+        "chefImageName": "urrechu.jpg",
+        "recipeImageName": "urrechu_receta.jpg",
+        "video": "ZEtM3oNp5m4"
+    },
+    {
+        "id": 4,
+        "name": "Iván Cerdeño",
+        "restaurant": "Restaurante Iván Cerdeño",
+        "recipe": "Roastbeef de lomo bajo con encurtidos y bearnesa",
+        "website": "https://ivancerdeno.com/",
+        "chefImageName": "ivancerdeno.jpg",
+        "recipeImageName": "ivancerdeno_receta.jpg",
+        "video": "IhgTLPndo6E"
+    },
+    {
+        "id": 5,
+        "name": "Javi Estévez",
+        "restaurant": "Restaurante La Tasquería",
+        "recipe": "Ensalada de lengua de ternera con mahonesa de atún y antxoa",
+        "website": "https://latasqueria.com/",
+        "chefImageName": "javiestevez.jpg",
+        "recipeImageName": "javiestevez_receta.jpg",
+        "video": "hY4s9Ca9tVQ",
+        
+    },
+    {
+        "id": 6,
+        "name": "MI A2",
+        "restaurant": "Xiaomi",
+        "recipe": "Great phone with an excellent interface. One of the best phones in the market.",
+        "website": "black",
+        "chefImageName": "Xiaomi_MI_A2.jpeg",
+        "recipeImageName": "davidmontero_receta.jpg",
+        "video": "5,99 inch Full-HD",
+        
+    },
+    {
+        "id": 7,
+        "name": "Moto G6",
+        "restaurant": "Motorola",
+        "recipe": "Great phone with an excellent interface. One of the best phones in the market.",
+        "website": "black",
+        "chefImageName": "Moto_G6.png",
+        "recipeImageName": "davidmontero_receta.jpg",
+        "video": "5,7 inch Full-HD",
+        
+    }
+  ]
