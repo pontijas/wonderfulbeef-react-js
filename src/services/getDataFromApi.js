14f03723f420f3fd7fb8c1cf7bea2e0a21da5e8b
root <root@DESKTOP-K4RHFBE.localdomain>
2021-01-29 19:56:33 +0100

    update chef json

--- src/services/getDataFromApi.js
+++ src/services/getDataFromApi.js
@@ -1,6 +1,6 @@
 [
     {
-        "id": 0,
+        "id": 00,
         "name": "David Montero",
         "restaurant": "Rice Paella",
         "recipe": "Arroz de solomillo con pasas y orejones",
@@ -10,38 +10,38 @@
         "video": "zhWtvFhLHvY"
     },
     {
-        "id": 1,
+        "id": 01,
         "name": "Fernando del Cerro",
         "restaurant": "Restaurante Casa Jose",
         "recipe": "Lomo de buey a la sal negra",
         "website": "http://casajose.es/",
         "chefImageName": "fernandodelcerro.jpg",
         "recipeImageName": "fernandodelcerro_receta.jpg",
-        "video": "x2LtwRnTEUc"
+        "video": "-d8bV04ePX4"
         
     },
     {
-        "id": 2,
+        "id": 02,
         "name": "Fran Martínez",
         "restaurant": "Restaurante Maralba",
         "recipe": "Canelones de carrillada con foie gras",
         "website": "https://www.maralbarestaurante.es/",
         "chefImageName": "franmartinez.jpg",
         "recipeImageName": "franmartinez_receta.jpg",
-        "video": "-eelhpeHfJA"
+        "video": "wQJTpRyS2KI"
     },
     {
-        "id": 3,
+        "id": 03,
         "name": "Iñigo Urrechu",
         "restaurant": "Restaurante Urrechu",
         "recipe": "Cachopo",
         "website": "https://urrechuvelazquez.com/",
         "chefImageName": "urrechu.jpg",
         "recipeImageName": "urrechu_receta.jpg",
-        "video": "ZEtM3oNp5m4"
+        "video": "4Os_k_AMgy4"
     },
     {
-        "id": 4,
+        "id": 04,
         "name": "Iván Cerdeño",
         "restaurant": "Restaurante Iván Cerdeño",
         "recipe": "Roastbeef de lomo bajo con encurtidos y bearnesa",
@@ -51,36 +51,91 @@
         "video": "IhgTLPndo6E"
     },
     {
-        "id": 5,
+        "id": 05,
         "name": "Javi Estévez",
         "restaurant": "Restaurante La Tasquería",
         "recipe": "Ensalada de lengua de ternera con mahonesa de atún y antxoa",
         "website": "https://latasqueria.com/",
         "chefImageName": "javiestevez.jpg",
         "recipeImageName": "javiestevez_receta.jpg",
-        "video": "hY4s9Ca9tVQ",
+        "video": "hY4s9Ca9tVQ"
         
     },
     {
-        "id": 6,
-        "name": "MI A2",
-        "restaurant": "Xiaomi",
-        "recipe": "Great phone with an excellent interface. One of the best phones in the market.",
-        "website": "black",
-        "chefImageName": "Xiaomi_MI_A2.jpeg",
-        "recipeImageName": "davidmontero_receta.jpg",
-        "video": "5,99 inch Full-HD",
+        "id": 06,
+        "name": "Javier Aranda",
+        "restaurant": "Restaurante Gaytán",
+        "recipe": "Solomillo relleno de verduras y foie",
+        "website": "https://chefjavieraranda.com/es/gaytan.html",
+        "chefImageName": "javieraranda.jpg",
+        "recipeImageName": "javieraranda_receta.jpg",
+        "video": "UHrdDoQOUMI"
         
     },
     {
-        "id": 7,
-        "name": "Moto G6",
-        "restaurant": "Motorola",
-        "recipe": "Great phone with an excellent interface. One of the best phones in the market.",
-        "website": "black",
-        "chefImageName": "Moto_G6.png",
-        "recipeImageName": "davidmontero_receta.jpg",
-        "video": "5,7 inch Full-HD",
+        "id": 07,
+        "name": "Joel Castanyé",
+        "restaurant": "Restaurante La Boscana",
+        "recipe": "Fricando de tapilla con setas de temporada",
+        "website": "https://www.laboscana.net/ca/",
+        "chefImageName": "joelcastanye.jpg",
+        "recipeImageName": "joelcastanye_receta.jpg",
+        "video": "JuoLqC852LA"
+        
+    },
+    {
+        "id": 08,
+        "name": "Juanlu Fernández",
+        "restaurant": "Restaurante Lú, Cocina y Alma",
+        "recipe": "Guiso de carrillada con puré de patata",
+        "website": "https://universolu.com/fr/les-restaurants/lu-cocina-y-alma/",
+        "chefImageName": "juanlufernandez.jpg",
+        "recipeImageName": "juanlufernandez_receta.jpg",
+        "video": "Wg1-ACwxF5Q"
+        
+    },
+    {
+        "id": 09,
+        "name": "Juanmi Carrasco",
+        "restaurant": "Restaurante Hermanos Carrasco",
+        "recipe": "Chuleta de vacuno a la brasa",
+        "website": "https://hermanoscarrasco.es/",
+        "chefImageName": "juanmicarrasco.jpg",
+        "recipeImageName": "juanmicarrasco_receta.jpg",
+        "video": "gkxUGCx9hSQ"
+        
+    },
+    {
+        "id": 10,
+        "name": "Kisco García",
+        "restaurant": "Restaurante Choco",
+        "recipe": "Royal de rabo de toro con cebolla de cacao",
+        "website": "https://www.restaurantechoco.com/",
+        "chefImageName": "kiscogarcia.jpg",
+        "recipeImageName": "kiscogarcia_receta.jpg",
+        "video": "0aq9ULs9cPM" 
+        
+    },
+    {
+        "id": 11,
+        "name": "Miguel Ángel de la Cruz",
+        "restaurant": "Restaurante La Botica de Matapozuelos",
+        "recipe": "Solomillo wellington con crema de castañas",
+        "website": "https://laboticadematapozuelos.com/",
+        "chefImageName": "miguelangeldelacruz.jpg",
+        "recipeImageName": "miguelangeldelacruz_receta.jpg",
+        "video": "fWkcXmKIDzc"
+        
+    },
+    {
+        "id": 12,
+        "name": "Rafa Centeno",
+        "restaurant": "Restaurante Maruja Limón",
+        "recipe": "Falda de ternera rellena con compota de manzana asada",
+        "website": "https://www.marujalimon.es/restaurante/",
+        "chefImageName": "rafacenteno.jpg",
+        "recipeImageName": "rafacenteno_receta.jpg",
+        "video": "BxtDV4Z02cE"
         
     }
   ]
